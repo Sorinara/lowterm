24ca553efd96c8c87c5346d4525479bada868490
Sorinara <playos@naver.com>
2014-05-13 14:08:02 +0900

    no use pager, deleted

--- module/mc.sh
+++
@@ -1,5 +0,0 @@
-clear
-\rm page_map.o page_map 2>/tmp/null
-gcc -Wall  -g -o page_map page_map.c && ./page_map
-#gcc -c page_map.c
-#gcc -Wall -g -o key_test key_test.c page_map.o `pkg-config --cflags --libs gtk+-2.0 gthread-2.0` -lX11 -I/usr/include/glib-2.0 -I/usr/lib/glib-2.0/include && ./key_test
